b303eaabd80b5044badf187556a604dacbcc8405
Иван <ivanpaskhin@yandex.ru>
2022-11-23 14:30:07 +0300

    Домашняя работа: ООП - 3

--- src/HomeWork_1/transport/Automobile.java
+++ src/HomeWork_1/transport/Automobile.java
@@ -1,12 +1,11 @@
 package HomeWork_1.transport;
 
-public class Automobile {
+public class Automobile extends Transport {
 
-    private String brand;
-    private String model;
+    //private String model;
     private double engineVolume;
     private String color;
-    private int year;
+    //private int year;
     private String country;
 
     private int gearBox;
@@ -48,22 +47,11 @@ public class Automobile {
     public Automobile(String brand, String model, double engineVolume, String color, int year, String country,
                       int gearBox,String bodyType,String regNumber,int availableSeats) {
 
-        if (brand == null || brand.equals("")) brand = "default";
-        if (model == null || model.equals("")) model = "default";
-        if (country == null || country.equals("")) country = "default";
+        super(brand,model,year,country,color,engineVolume);
 
         if (engineVolume <= 0) engineVolume = 1.5;
 
-        if (color == null || color.equals("")) color = "Белый";
-
-        if (year <= 0) year = 2000;
-
-        this.brand = brand;
-        this.model = model;
         this.engineVolume = engineVolume;
-        this.color = color;
-        this.year = year;
-        this.country = country;
 
         if (gearBox <= 0) gearBox = 4;
         if (availableSeats <= 0) availableSeats = 4;
@@ -74,30 +62,11 @@ public class Automobile {
         this.bodyType = bodyType;
         this.regNumber = regNumber;
         this.availableSeats = availableSeats;
-    }
 
-    public String getBrand() {
-        return brand;
-    }
-
-    public String getModel() {
-        return model;
     }
 
     public double getEngineVolume() {
         return engineVolume;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public String getCountry() {
-        return country;
     }
 
     public int getGearBox() {
@@ -119,15 +88,18 @@ public class Automobile {
     private boolean tiresType(boolean isWinterTires){
         return isWinterTires;
     }
+    public void refill(int fuelType) {
+        super.refill(fuelType);
+    }
 
     @Override
     public String toString() {
-        return "Автомобиль: " + brand +
-                ", модель: " + model +
+        return "Автомобиль: " + getBrand() +
+                ", модель: " + getModel() +
                 ", обьём двигателя: " + engineVolume +
-                ", цвет: " + color +
-                ", год производства: " + year +
-                ", страна сборки: " + country +
+                ", цвет: " + getColor() +
+                ", год производства: " + getYear() +
+                ", страна сборки: " + getCountry() +
                 ", коробка передач: " + gearBox +
                 ", тип кузова: " + bodyType +
                 ", регистрационный номер: " + regNumber +
